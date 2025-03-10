c5ffd8c59ea6622921fb46bd38f0f132a8eef34f
avanishihora48 <163129933+avanishihora48@users.noreply.github.com>
2025-03-10 19:29:14 +0530

    commit

---
+++ Js_practice/Projects/project6/keyboard.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `<div>
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <th>${e.key === ' ' ? 'space' : e.key}</th>
+    <th>${e.keyCode}</th>
+    <th>${e.code}</th>
+  </tr>
+  </table>
+  </div>`
+})
